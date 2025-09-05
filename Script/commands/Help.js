e6db13150356dcb1eb9ecc9e823c7cb24cf94a91
hridoy82764248 <hridoykhan827642488@yahoo.com>
2025-09-05 22:18:34 +0600

    Update Help.js

--- Script/commands/Help.js
+++ Script/commands/Help.js
@@ -44,7 +44,7 @@ module.exports.languages = {
 // এখানে আপনার ফোটো Imgur লিংক করে বসাবেন✅
 
 const helpImages = [
-    "https://i.imgur.com/sxSn1K3.jpeg"
+    "https://imgur.com/gallery/huh-L2HybF6"
 ];
 
 function downloadImages(callback) {
